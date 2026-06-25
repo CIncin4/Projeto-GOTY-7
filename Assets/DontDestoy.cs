2c464fd64a360744a1d670c008d0de3e8407388d
CIncin4 <0cincin0x@gmail.com>
2026-06-25 17:07:30 -0300

    Main-V0.6.2
    Nave nao destroy

---
+++ Assets/DontDestoy.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class DontDestoy : MonoBehaviour
+{
+    private static GameObject[] persistentObject = new GameObject[1];
+    public int objectIndex;
+    void Awake()
+    {
+        if (persistentObject[objectIndex] == null)
+        {
+            persistentObject[objectIndex] = gameObject;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (persistentObject[objectIndex] != gameObject)
+            Destroy(gameObject);
+    }
+}
